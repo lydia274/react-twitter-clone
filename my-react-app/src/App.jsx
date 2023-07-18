25a091f6f4960c49e8ba2c3dd67aebeae3615ca6
lydia a <lydie.antonova@gmail.com>
2023-07-18 12:49:47 +0200

    basic functionality and like counter

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
 import Tweet from "./Tweet"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>coursera - joposhniki</h1>
--- my-react-app/src/Tweet.jsx
+++ my-react-app/src/Tweet.jsx
@@ -1,6 +1,15 @@
 import React from "react"
+import { useState } from "react"
 
 function Tweet(props) {
+  const [counter, setCounter] = useState(0)
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
+
   const text = props.text
   const author = props.author
   const date = props.date
@@ -10,6 +19,8 @@ function Tweet(props) {
       <p>
         by {author} on {date}
       </p>
+      <h6>Likes: {counter}</h6> <button onClick={increment}>+1</button>{" "}
+      <button onClick={decrement}>-1</button>
     </div>
   )
 }

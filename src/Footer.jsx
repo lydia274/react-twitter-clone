f798a800f3fd5220b439614bb398fcf28e56c72d
lydia a <lydie.antonova@gmail.com>
2023-07-24 18:21:40 +0200

    updated structure

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <div>
+      <div>about the project</div>
+      <div>contact me</div>
+      <div>post apocalyptic</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>More</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
